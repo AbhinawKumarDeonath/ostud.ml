099adaa2f076ad3c01727414654e404d7a21bf7f
AbhinawKumarDeonath <95974692+AbhinawKumarDeonath@users.noreply.github.com>
2022-01-14 15:57:27 +0530

    adding contact us page

---
+++ contact.php
@@ -0,0 +1,20 @@
+<?php
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
+
+
+    $email_from ='abhinawdeonath30@gmail.com';
+    $email_subject = " new form submission";
+
+    $email_body = "User Name: $name.\n"\.
+                  "User Email: $visitor_email.\n".
+                  "User Message: $message.\n";
+
+    $to = "abhinawdeonath30@gmail.com";
+    $headers = " From: $email_from \r\n";
+    $headers = " Reply-to: $visitor_email \r\n";
+    mail($to,$email_subject,$email_body,$headers);
+    header("Location:contact.html");
+
+    ?>
